77218ab1862fba101677ade1dec6517a6b8fe713
Julien Ergan <julien.ergan.code@gmail.com>
2020-09-03 16:16:58 +0900

    implemented giphy-api search function

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
 import SearchBar from './search_bar';
 import Gif from './gif';
 import GifList from './gif_list';
 
 class App extends Component {
-  render() {
-    const gifs = [
-      { id: "xT9IgDEI1iZyb2wqo8" },
-      { id: "xT9IgDEI1iZyb2wqo8" }
-    ];
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
+    }
+    // this.search("disney");
+  }
+
+  search = (query) => {
+    // TODO: API call
+    giphy('EfieXGVZLa24MvZu0SB60Tfwtqx1BYE1').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (err, res) => {
+      // Res contains gif data!
+      // console.log(res);
+      this.setState({
+        gifs: res.data
+      });
+      });
+  }
+
+  render() {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
         </div>
         <div className="selected-gif">
-          <Gif id="xT9IgDEI1iZyb2wqo8" />
+          <Gif id={this.state.selectedGifId} />
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import Gif from './gif';
 
 class GifList extends Component {
-  render() {
 
-      this.props.gifs.map(gif => {
-        return <Gif id={gif.id} />
-      });
-
+  renderList = () => {
+    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} /> );
   }
+
+  render() {
+    return (
+      <div className="gif-list">
+        {this.renderList()}
+      </div>
+    );
+  } 
 }
 
 export default GifList;
