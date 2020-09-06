b4a2327c013a3b76ae70feea58ea07911db81807
Julien Ergan <julien.ergan.code@gmail.com>
2020-09-06 21:04:56 +0900

    SearchBar as an example of a controlled component

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ""
+    }
+  }
   
   handleUpdate = (event) => {
     // console.log(event.target.value);
     // debugger
+    this.setState({
+      term: event.target.value
+    });
     this.props.searchFunction(event.target.value);
   }
 
   render() {
     return (
-      <input type="text" className="form-control form-search"
-      onChange={this.handleUpdate} />
+      <input 
+        value={this.state.term}
+        type="text" 
+        className="form-control form-search"
+        onChange={this.handleUpdate} 
+      />
     );
   }
 }
